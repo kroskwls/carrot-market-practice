d80c37c11a4009b6a3c3e891207eb07147c0d73d
kroskwls <kroskwls@gmail.com>
2024-06-23 15:39:04 +0900

    add home, login, join page

---
+++ components/form-button.tsx
@@ -0,0 +1,13 @@
+interface FormButtonProps {
+	text: string
+	loading: boolean
+}
+
+export default function FormButton({ text, loading }: FormButtonProps) {
+	return (
+		<button
+			className="primary-btn h-10 disabled:bg-neutral-400 disabled:text-neutral-300 disabled:cursor-not-allowed"
+			disabled={loading}
+		>{text}</button>
+	);
+}
